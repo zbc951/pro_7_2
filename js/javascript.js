6208478de64e99ff6bea24eb80bc67711318eb41
lazio@wataidesign.com <lazio@wataidesign.com>
2020-10-05 17:47:08 +0800

    1091005

--- js/javascript.js
+++ js/javascript.js
@@ -11,7 +11,36 @@ $(document).on('ready', function () {
 	})
 })
 
-//下選單動畫
+//nav選單效果
+$(function(){
+    $('.lang1').click(function(){
+        $('nav').attr('class','navisited1')
+    })
+    $('.lang2').click(function(){
+        $('nav').attr('class','navisited2')
+    })
+    $('.lang3').click(function(){
+        $('nav').attr('class','navisited3')
+    })
+})
+
+//nav出現
+$(function(){
+    $('.topic').click(function(){
+        $('body').addClass('navslide')
+    })
+    $('.navic').click(function(){
+        $('body').removeClass('navslide')
+    })
+    $('.blackbg').click(function(){
+        $('body').removeClass('navslide')
+    })
+    $('.navlist li').click(function(){
+        $('body').removeClass('navslide')
+    })
+})
+
+//下選單按鈕效果
 $(function(){
     $('.but1').click(function(){
         $('main').attr('class','visited1')
@@ -30,5 +59,47 @@ $(function(){
     })
     $('.but6').click(function(){
         $('main').attr('class','visited6')
+    })
+})
+
+//signup顯示密碼按鈕
+$(function(){
+    $('.passwordbut').mouseenter(function(){
+        $('.password').attr('type','text')
+    })
+    $('.passwordbut').mouseleave(function(){
+        $('.password').attr('type','password')
+    })
+})
+
+//signup出現
+$(function(){
+    $('.b2').click(function(){
+        $('body').addClass('signupapp')
+    })
+    $('.blackbg').click(function(){
+        $('body').removeClass('signupapp')
+    })
+    $('.conbut').click(function(){
+        $('body').removeClass('signupapp')
+    })
+    $('.inli2').click(function(){
+        $('body').attr('class','signupapp')
+    })
+})
+
+//login出現
+$(function(){
+    $('.b1').click(function(){
+        $('body').addClass('loginapp')
+    })
+    $('.blackbg').click(function(){
+        $('body').removeClass('loginapp')
+    })
+    $('.conbut').click(function(){
+        $('body').removeClass('loginapp')
+    })
+    $('.upli1').click(function(){
+        $('body').attr('class','loginapp')
     })
 })
